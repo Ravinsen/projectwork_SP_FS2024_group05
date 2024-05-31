703edebeb8bd7be83da15dbf5941d1d8f8282f94
Ravinsen <ravinsen@students.zhaw.ch>
2024-05-31 10:29:23 +0200

    Update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import pandas as pd
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -14,7 +15,31 @@ def get_data():
 @app.route('/')
 def index():
     df = get_data()
-    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('index.html', table=df.to_html(classes='table table-striped table-bordered table-hover', index=False))
+
+@app.route('/chart1')
+def chart1():
+    return render_template('chart.html', chart='1. Bar plot_Total Population by continent.png')
+
+@app.route('/chart2')
+def chart2():
+    return render_template('chart.html', chart='2. Histogram_Population Growth Rates.png')
+
+@app.route('/chart3')
+def chart3():
+    return render_template('chart.html', chart='3. Pie chart_Population Categories.png')
+
+@app.route('/chart4')
+def chart4():
+    return render_template('chart.html', chart='4. Scatter Plot_Population vs Density.png')
+
+@app.route('/chart5')
+def chart5():
+    return render_template('chart.html', chart='5. Scatter Plot_Population vs Growth Rate.png')
+
+@app.route('/Charts/<path:filename>')
+def send_chart(filename):
+    return send_from_directory('Charts', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
